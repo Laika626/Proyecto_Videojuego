da0863ec2cf5bc466a77f47fb109d20840da23b5
Laika626 <cocolylo@Fernandas-MacBook-Air-686.local>
2026-09-18 12:13:19 -0600

    Aumenta la velocidad de los fantasmas

--- pacman.py
+++ pacman.py
@@ -20,10 +20,10 @@ writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(8, 0)],
+    [vector(-180, -160), vector(0, 8)],
+    [vector(100, 160), vector(0, -8)],
+    [vector(100, -160), vector(-8, 0)],
 ]
 # fmt: off
 tiles = [
@@ -135,11 +135,11 @@ def move():
             point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
-            ]
+		vector(8, 0),
+    		vector(-8, 0),
+   		vector(0, 8),
+    		vector(0, -8),
+	    ]
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
